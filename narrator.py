8fc5f767c70da3fa4c0429e28bf04ba10aa3e0a9
John Goyette <jagoyette@gmail.com>
2023-11-18 15:30:14 -0500

    Reverted change to json

--- narrator.py
+++ narrator.py
@@ -54,7 +54,7 @@ def generate_new_line(base64_image):
                 {"type": "text", "text": "Describe this image"},
                 {
                     "type": "image_url",
-                    "image_url": f"data:image/jpeg;base64,{base64_image}"
+                    "image_url": f"data:image/jpeg;base64,{base64_image}",
                 },
             ],
         },
